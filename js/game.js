1b38cb208e3f6d729a00bd9bbb39f2ffb6ecb844
Joseph Hill <Zyrconium@users.noreply.github.com>
2014-12-07 19:47:30 +0000

    Updated game.js

--- js/game.js
+++ js/game.js
@@ -1,39 +1,55 @@
 var total = 0;
 var totalText;
-var min = 0;
-var max = 100;
-var time = 5000;
 var lastIncrement = Date.now();
 var boostText;
+var critIndicatorText;
+
+var min = 0;
 var minText;
 var incMinCost = 1;
 var incMinCostText;
+
+var max = 100;
 var maxText;
 var incMaxCost = 200;
 var incMaxCostText;
+
 var critChance = 0.00;
 var CritChanceText;
-var incCritChanceCost = 100;
+var incCritChanceCost = 1000;
 var incCritChanceCostText;
+
 var critMulti = 2;
 var CritMultiText;
 var incCritMultiCost = 25;
 var incCritMultiCostText;
 
+var time = 5000;
+var timeText;
+var decTimeCost = 500;
+var decTimeCostText;
+
 window.addEventListener("load", function() {
 	// call functions and make things happen here, and nowhere else
+	
 	totalText = document.getElementById("totalPoints");
 	boostText = document.getElementById("lastAddition");
+	critIndicatorText = document.getElementById("critIndicator");
+	
 	minText = document.getElementById("minimum");
 	maxText = document.getElementById("maximum");
 	critChanceText = document.getElementById("critChance");
 	critMultiText = document.getElementById("critMulti");
+	timeText = document.getElementById("time");
+	
 	incMinCostText = document.getElementById("incMinCost");
 	incMaxCostText = document.getElementById("incMaxCost");
 	incCritChanceCostText = document.getElementById("incCritChanceCost");
 	incCritMultiCostText = document.getElementById("incCritMultiCost");
+	decTimeCostText = document.getElementById("decTimeCost");
+	
 	setupButtons();
-	setInterval(randomBoost, time);
+	setTimeout(randomBoost, time);
 	tick();
 })
 
@@ -42,6 +58,7 @@ function setupButtons() {
 	var incMax = document.getElementById("incMax");
 	var incCritChance = document.getElementById("incCritChance");
 	var incCritMulti = document.getElementById("incCritMulti");
+	var decTime = document.getElementById("decTime");
 	var totalText = document.getElementById("totalPoints");
 	incMin.addEventListener("click", function(event) {
 		if((total>incMinCost)&&(min<max))
@@ -66,12 +83,12 @@ function setupButtons() {
 		}
 	});
 	incCritChance.addEventListener("click", function(event) {
-		if((total>incCritChanceCost))
+		if((total>incCritChanceCost) && (critChance<100))
 		{
 			critChance += 0.01;
-			critChanceText.innerHTML = critChance.toFixed(2);
+			critChanceText.innerHTML = (critChance*100).toFixed(0);
 			total -= incCritChanceCost;
-			incCritChanceCost = (critChance*100)+100;
+			incCritChanceCost = (((critChance*100)+100)*10).toFixed(0);
 			incCritChanceCostText.innerHTML = incCritChanceCost;
 			totalText.innerHTML = total;
 		}
@@ -87,23 +104,38 @@ function setupButtons() {
 			totalText.innerHTML = total;
 		}
 	});
+	decTime.addEventListener("click", function(event) {
+		if((total>decTimeCost)&&(time>1000))
+		{
+			time -= 1000;
+			var simpletime = time/1000;
+			timeText.innerHTML = simpletime;
+			total -= decTimeCost;
+			decTimeCost = Math.pow((simpletime*100), (6-simpletime));
+			decTimeCostText.innerHTML = decTimeCost;
+			totalText.innerHTML = total;
+		}
+	});
 };
 
 function randomBoost() {
 
 	lastIncrement = Date.now();
-	if((Math.random()*100)<critChance)
+	if((Math.random())<critChance)
 	{
 		boost = (Math.ceil(Math.random()*(max - min)) + min)*critMulti;
+		critIndicatorText.innerHTML = "CRITICAL GAINS!";
 	}
 	else
 	{
 		boost = Math.ceil(Math.random()*(max - min)) + min;
+		critIndicatorText.innerHTML = "";
 	}
 	total += boost;
 	totalText.innerHTML = total;
 	boostText.innerHTML = boost;
 	
+	setTimeout(randomBoost, time);
 }
 
 function tick() {
